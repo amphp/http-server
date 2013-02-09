1e2cedd9c2d3541e1d444099edf9cd452043a520
Daniel Lowrey <rdlowrey@gmail.com>
2013-02-09 12:22:09 -0500

    mod.limit tweaks

--- src/Aerys/Mods/Limit.php
+++ src/Aerys/Mods/Limit.php
@@ -8,61 +8,65 @@ use Aerys\Server,
 /*
 'mod.limit' => [
     'ipProxyHeader' => NULL, // use the specified header instead of the raw IP if available
+    'onLimitCmd' => NULL,
+    'onLimitCallback' => NULL,
     'limits' => [
         60 => 100,
-        3600 => 2500,
-        86400 => 5000
+        3600 => 2500
     ]
 ],
 */
 
 class Limit implements OnRequestMod {
-    
-    const RATE_LIMIT_CLEANUP_INTERVAL = 10000000;
-    
+
     private $server;
+    
     private $ipProxyHeader;
     private $rateLimits = [];
     private $ratePeriods = [];
     private $rateAllowances = [];
-    private $lastRateCheckTimes = [];
+    private $lastRateCheckedAt = [];
     private $maxRatePeriod;
+    
+    private $onLimitCmd;
+    private $onLimitCallback;
     
     static function createMod(Server $server, EventBase $eventBase, array $config) {
         $class = __CLASS__;
-        return new $class($server, $eventBase, $config);
+        return new $class($server,$config);
     }
     
-    function __construct(Server $server, EventBase $eventBase, array $config) {
+    function __construct(Server $server, array $config) {
         $this->server = $server;
-        $this->configure($config, $eventBase);
+        $this->configure($config);
     }
     
-    private function configure(array $config, EventBase $eventBase) {
+    private function configure(array $config) {
         if (!empty($config['ipProxyHeader'])) {
             $this->ipProxyHeader = strtoupper($config['ipProxyHeader']);
+        }
+        if (!empty($config['onLimitCmd'])) {
+            $this->onLimitCmd = $config['onLimitCmd'];
+        }
+        if (!empty($config['onLimitCallback']) && is_callable($config['onLimitCallback'])) {
+            $this->onLimitCallback = $config['onLimitCallback'];
+        }
+        if (empty($config['limits'])) {
+            throw new \DomainException(
+                'No rate limits specified'
+            );
         }
         
         ksort($config['limits']);
         $this->maxRatePeriod = max(array_keys($config['limits']));
         $this->rateLimits = $config['limits'];
-        
-        foreach (array_keys($config['limits']) as $period) {
-            $this->ratePeriods[] = $period;
-            $this->rateAllowances[$period] = [];
-            $this->lastRateCheckTimes[$period] = [];
-        }
-        
-        $eventBase->repeat(self::RATE_LIMIT_CLEANUP_INTERVAL, function() {
-            $this->clearExpiredRateLimitData(time());
-        });
-        
+        $this->ratePeriods[] = array_keys($config['limits']);
     }
     
     /**
      * @param int $clientId
      * @param int $requestId
-     * @return bool Returns FALSE if rate limited to prevent further onRequest mod processing
+     * @return void
      */
     function onRequest($clientId, $requestId) {
         $asgiEnv = $this->server->getRequest($requestId);
@@ -76,41 +80,51 @@ class Limit implements OnRequestMod {
         }
         
         $now = time();
+        
         if ($this->isRateLimited($clientIp, $now)) {
-            $this->limit($requestId, $clientIp);
+            $this->limit($requestId);
+            if ($this->onLimitCmd) {
+                $this->execSystemCmd($clientIp);
+            }
+            if ($callback = $this->onLimitCallback) {
+                try {
+                    $callback($clientIp);
+                } catch (\Exception $e) {
+                    // @TODO Handle exception
+                }
+            }
         }
         
         $this->clearExpiredRateLimitData($now);
     }
     
     private function isRateLimited($clientIp, $now) {
+        if (isset($this->lastRateCheckedAt[$clientIp])) {
+            $elapsedTime = $now - $this->lastRateCheckedAt[$clientIp];
+            // move the clientIp check timestamp to the end of the ordered array for timed removal
+            unset($this->lastRateCheckedAt[$clientIp]);
+        } else {
+            $elapsedTime = 0;
+            $this->rateAllowances[$clientIp] = $this->rateLimits;
+        }
+        
+        $this->lastRateCheckedAt[$clientIp] = $now;
+        
+        // NOTICE: We are operating on this value BY REFERENCE.
+        $allowances =& $this->rateAllowances[$clientIp];
+        
         foreach ($this->rateLimits as $period => $rate) {
-            if (!isset($this->rateAllowances[$period][$clientIp])) {
-                $this->rateAllowances[$period][$clientIp] = $rate;
-            }
-            
-            // NOTICE: We are operating on this value BY REFERENCE.
-            $allowance =& $this->rateAllowances[$period][$clientIp];
-            
-            if (isset($this->lastRateCheckTimes[$clientIp])) {
-                $elapsedTime = (float) ($now - $this->lastRateCheckTimes[$clientIp]);
-                unset($this->lastRateCheckTimes[$clientIp]);
-            } else {
-                $elapsedTime = 0;
-            }
-            
-            $this->lastRateCheckTimes[$clientIp] = $now;
-            
-            $allowance += $elapsedTime * ($rate / $period);
+            $allowances[$period] += $elapsedTime * ($rate / $period);
             
             // Throttle (because you can't save up "rate credit" beyond the max)
-            if ($allowance > $rate) {
-                $allowance = $rate;
+            if ($allowances[$period] > $rate) {
+                $allowances[$period] = $rate;
             }
             
-            // All requests, even rate-limited requests count against your average rate
-            $allowance -= 1.0;
-            if ($allowance < 0.0) {
+            // All requests (even rate-limited requests) count against your average rate
+            $allowances[$period] -= 1.0;
+            
+            if ($allowances[$period] < 0.0) {
                 return TRUE;
             }
         }
@@ -118,8 +132,8 @@ class Limit implements OnRequestMod {
         return FALSE;
     }
     
-    private function limit($requestId, $clientIp) {
-        $body = "<html><body><h1>429 Too Many Requests</h1><p>IP: $clientIp</p></body></html>";
+    private function limit($requestId) {
+        $body = "<html><body><h1>429 Too Many Requests</h1></body></html>";
         $headers = [
             'Content-Type' => 'text\html',
             'Content-Length' => strlen($body)
@@ -129,15 +143,25 @@ class Limit implements OnRequestMod {
         $this->server->setResponse($requestId, $asgiResponse);
     }
     
+    private function execSystemCmd($clientIp) {
+        $cmd = escapeshellcmd($this->onLimitCmd . ' ' . escapeshellarg($clientIp));
+        
+        if (substr(php_uname(), 0, 7) == "Windows") {
+            pclose(popen("start /B ". $cmd, "r"));
+        } else {
+            exec($cmd . " > /dev/null &");
+        }
+    }
+    
     private function clearExpiredRateLimitData($now) {
         $cutoffTime = $now - $this->maxRatePeriod;
         
-        foreach ($this->lastRateCheckTimes as $clientIp => $lastCheckedAt) {
+        foreach ($this->lastRateCheckedAt as $clientIp => $lastCheckedAt) {
             if ($lastCheckedAt < $cutoffTime) {
-                unset($this->lastRateCheckTimes[$clientIp]);
-                foreach ($this->ratePeriods as $period) {
-                    unset($this->rateAllowances[$period][$clientIp]);
-                }
+                unset(
+                    $this->lastRateCheckedAt[$clientIp],
+                    $this->rateAllowances[$clientIp]
+                );
             } else {
                 break;
             }
