9ee964d2302ed756461a84f5033d90716d51f19f
Daniel Lowrey <rdlowrey@gmail.com>
2013-02-12 23:37:32 -0500

    Updated filesys handler example

--- examples/file_server.php
+++ examples/file_server.php
@@ -3,9 +3,20 @@
 /**
  * examples/file_server.php
  * 
- * Aerys comes with a built-in filesystem handler for serving static files from a specified
- * document root. This feature is more useful as a secondary host in a multi-host environment, but
- * this example is helpful nonetheless.
+ * Aerys comes with a built-in filesystem request handler for serving static files from a specified
+ * document root. Caching headers are attached to all responses sent and all valid HTTP/1.1
+ * conditional headers are supported when responding to requests:
+ * 
+ * If-Match
+ * If-Modified-Since
+ * If-None-Match
+ * If-Range
+ * If-Unmodified-Since
+ * 
+ * Additionally, the Filesys handler fully supports byte range requests via the `Range:` request
+ * header. Multiple byte ranges are also supported using the `multipart/byteranges` content type.
+ * 
+ * To use this example, simply execute the file using your system's php binary:
  * 
  * $ php file_server.php
  * 
