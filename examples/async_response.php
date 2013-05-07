dec141bde9fa3263fea34c48c36cafb6c4c3af8d
Daniel Lowrey <rdlowrey@gmail.com>
2013-05-07 12:55:23 -0400

    Fixed async_response example bug from undeclared variable, updated docs

--- examples/async_response.php
+++ examples/async_response.php
@@ -3,18 +3,15 @@
 /**
  * examples/async_response.php
  * 
- * FYI: This is the single most important example in the repository. For reals.
- * 
- * So we know that the ASGI specification requires servers to invoke application callables with
- * the ASGI environment array. The application must then respond with the appropriate response
- * array. This basic IO approach works fine until you realize that Aerys executes inside a
- * non-blocking event loop. We also know that PHP is not a threaded language. Uh oh. If our application
- * code blocks for every response it only takes one slow database query (or any other IO-bound
- * operation to hose performance for the entire server. We're totally foobar'd.
+ * We know that the ASGI specification requires servers to invoke application callables with the
+ * ASGI environment array. The application must then respond with the appropriate response array.
+ * This basic IO approach works fine until you realize that Aerys executes inside a non-blocking
+ * event loop. We also know that PHP is not a threaded language. Uh oh. If our application code
+ * blocks for every response it only takes one slow database query (or any other IO-bound operation)
+ * to hose performance for the entire server. We're totally foobar'd.
  * 
  * Fortunately, we can invoke functions asynchronously using the Amp library's language-agnostic
- * async dispatcher to avoid blocking server execution while we wait for IO-bound operations like 
- * database queries to return. Currently asynchronous adapters exist to call both PHP and Python
+ * async dispatcher. Currently, asynchronous adapters exist to call both PHP and Python
  * functions asynchronously, though any language may be used with an appropriate adapter.
  * 
  * From here it's little more than a hop, skip and a jump to generating complex responses
@@ -30,13 +27,16 @@ use Aerys\Config\Configurator,
 require dirname(__DIR__) . '/autoload.php';
 require __DIR__ . '/support/async_response/ExampleAsyncApp.php';
 
-(new Configurator($injector))->createServer([[
+(new Configurator)->createServer([[
     'listenOn'    => '*:1337',
     'application' => new AsyncClassLauncher([
-        'handlerClass'  => 'ExampleAsyncApp', // A request handler class that typehints Amp\Async\Dispatcher
-        'functions'     => __DIR__ . '/support/async_response/example_async_functions.php', // A file containing functions to invoke asynchronously
+        'handlerClass'  => 'ExampleAsyncApp', // A handler class typehinting Amp\Async\Dispatcher in its constructor
+        'functions'     => __DIR__ . '/support/async_response/example_async_functions.php', // A file declaring functions you want to call asynchronously
+        
+        // --- OPTIONAL VALUES BELOW: we only declare these here to show that they exist --- //
+        
         'binaryCmd'     => NULL, // Defaults to the value of the PHP_BINARY constant
-        'workerCmd'     => NULL, // Don't specify unless you know what you're doing
+        'workerCmd'     => NULL, // The AMP worker script. Don't specify unless you know what you're doing.
         'processes'     => NULL, // How many worker processes to use in the dispatcher pool (defaults to 8)
         'callTimeout'   => NULL, // Return an error if an async call doesn't come back in X seconds (defaults to 30)
     ])
