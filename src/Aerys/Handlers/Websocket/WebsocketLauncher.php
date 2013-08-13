5cced4d3e62614a2663c3a38ebd31aec414c60b9
Daniel Lowrey <rdlowrey@gmail.com>
2013-08-13 00:36:54 -0400

    Fixed broken websocket host launcher

--- src/Aerys/Handlers/Websocket/WebsocketLauncher.php
+++ src/Aerys/Handlers/Websocket/WebsocketLauncher.php
@@ -8,37 +8,46 @@ use Auryn\Injector,
     Aerys\Config\ConfigException;
 
 class WebsocketLauncher extends ConfigLauncher {
-    
+
     private $handlerClass = 'Aerys\Handlers\Websocket\WebsocketHandler';
-    
+
     function launch(Injector $injector) {
-        
         $handler = $injector->make($this->handlerClass);
         $injector->share($handler);
-        $endpoints = $this->makeEndpoints($injector, $config);
+        
+        $endpoints = $this->makeEndpoints($injector);
+        
+        foreach ($endpoints as $uri => $endpointArr) {
+            $endpoint = $endpointArr['endpoint'];
+            unset($endpointArr['endpoint']);
+            $options = $endpointArr;
+            $handler->registerEndpoint($uri, $endpoint, $options);
+        }
+        
         $injector->unshare(get_class($handler));
+
+        return $handler;
     }
-    
+
     private function makeEndpoints(Injector $injector) {
         try {
             $endpoints = [];
-            
+
             foreach ($this->getConfig() as $requestUri => $endpointArr) {
                 if (isset($endpointArr['endpoint']) && is_string($endpointArr['endpoint'])) {
                     $endpointArr['endpoint'] = $injector->make($endpointArr['endpoint']);
                     $endpoints[$requestUri] = $endpointArr;
                 }
             }
-            
+
             return $endpoints;
-            
+
         } catch (InjectionException $injectionError) {
             throw new ConfigException(
-                'Failed injecting websocket dependencies', 
+                'Failed injecting websocket endpoint dependencies',
                 $errorCode = 0,
                 $injectionError
             );
         }
     }
 }
-
