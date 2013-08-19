ff2f7e3cb059ab6aa630c3c42aa3be8af12856df
Daniel Lowrey <rdlowrey@gmail.com>
2013-08-18 23:19:44 -0400

    Moved convenience autoloader into vendor directory to play nice with composer

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,12 +1,12 @@
 <?php
 
-require __DIR__ . '/vendor/Auryn/autoload.php';
-require __DIR__ . '/vendor/Alert/autoload.php';
+require __DIR__ . '/Auryn/autoload.php';
+require __DIR__ . '/Alert/autoload.php';
 
 spl_autoload_register(function($class) {
     if (0 === strpos($class, 'Aerys\\')) {
         $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-        $file = __DIR__ . "/src/$class.php";
+        $file = __DIR__ . "/../src/$class.php";
         if (file_exists($file)) {
             require $file;
         }
