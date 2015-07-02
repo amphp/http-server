8fff0269174dcb8f850c63f28c1b3478f4c24ad3
Bob Weinand <bobwei9@hotmail.com>
2015-07-02 19:58:38 +0200

    Fix Websocket Handshaker for already sent messages

--- lib/Websocket/Handshake.php
+++ lib/Websocket/Handshake.php
@@ -125,7 +125,7 @@ class Handshake implements Response {
             $this->handshake();
         }
         $this->isStarted = true;
-        $this->response->send("");
+        $this->response->end();
         return $this;
     }
 
