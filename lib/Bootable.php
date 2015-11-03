d31638ffe6098c731f8718652bcaccd4d0c37636
Bob Weinand <bobwei9@hotmail.com>
2015-11-03 16:32:04 +0100

    Use a proper phpdoc syntax

--- lib/Bootable.php
+++ lib/Bootable.php
@@ -3,6 +3,8 @@
 namespace Aerys;
 
 interface Bootable {
-    /** May return an instance of Middleware and/or callable to be used instead of the class implementing Bootable (which may also implement Middleware and/or be callable) */
+    /**
+     * @return Middleware|callable|null to be used instead of the class implementing Bootable (which may also implement Middleware and/or be callable)
+     */
     function boot(Server $server, Logger $logger);
 }
