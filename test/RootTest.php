bf9b55c4faef80582e1e1ca1ae2d353bcd0807cd
Bob Weinand <bobwei9@hotmail.com>
2015-11-11 22:29:47 +0100

    Merge some old root tests

--- test/RootTest.php
+++ test/RootTest.php
@@ -2,6 +2,8 @@
 
 namespace Aerys\Test;
 
+use Aerys\Server;
+use Aerys\ServerObserver;
 use Amp\File as file;
 
 class RootTest extends \PHPUnit_Framework_TestCase {
@@ -14,12 +16,12 @@ class RootTest extends \PHPUnit_Framework_TestCase {
      */
     public static function setUpBeforeClass() {
         $fixtureDir = self::fixturePath();
-        if (!\mkdir($fixtureDir, $mode = 0777, $recursive = true)) {
+        if (!file_exists($fixtureDir) && !\mkdir($fixtureDir)) {
             throw new \RuntimeException(
                 "Failed creating temporary test fixture directory: {$fixtureDir}"
             );
         }
-        if (!\mkdir($fixtureDir . "/dir", $mode = 0777, $recursive = true)) {
+        if (!file_exists($fixtureDir. "/dir") &&!\mkdir($fixtureDir . "/dir")) {
             throw new \RuntimeException(
                 "Failed creating temporary test fixture directory"
             );
@@ -49,9 +51,8 @@ class RootTest extends \PHPUnit_Framework_TestCase {
      * @expectedExceptionMessage Document root requires a readable directory
      */
     public function testConstructorThrowsOnInvalidDocRoot($badPath) {
-        $reactor = $this->getMock("Amp\\Reactor");
-        $filesystem = $this->getMock("Amp\\File\\Driver");
-        $root = new \Aerys\Root($badPath, $filesystem, $reactor);
+        $filesystem = $this->getMock('Amp\File\Driver');
+        $root = new \Aerys\Root($badPath, $filesystem);
     }
 
     public function provideBadDocRoots() {
@@ -65,9 +66,8 @@ class RootTest extends \PHPUnit_Framework_TestCase {
      * @dataProvider provideRelativePathsAboveRoot
      */
     public function testForbiddenResponseOnRelativePathAboveRoot($relativePath) {
-        $reactor = $this->getMock("Amp\\Reactor");
-        $filesystem = $this->getMock("Amp\\File\\Driver");
-        $root = new \Aerys\Root(self::fixturePath(), $filesystem, $reactor);
+        $filesystem = $this->getMock('Amp\File\Driver');
+        $root = new \Aerys\Root(self::fixturePath(), $filesystem);
         $request = $this->getMock("Aerys\\Request");
         $request->expects($this->once())
             ->method("getUri")
@@ -87,44 +87,116 @@ class RootTest extends \PHPUnit_Framework_TestCase {
             ["/dir/../../"],
         ];
     }
+
+    public function testBasicFileResponse() {
+        $root = new \Aerys\Root(self::fixturePath());
+        $request = $this->getMock('Aerys\Request');
+        $request->expects($this->once())
+            ->method("getUri")
+            ->will($this->returnValue("/test.txt"))
+        ;
+        $request->expects($this->any())
+            ->method("getMethod")
+            ->will($this->returnValue("GET"))
+        ;
+        $response = $this->getMock('Aerys\Response');
+        $response->expects($this->once())
+            ->method("end")
+            ->with("test") // <-- the contents of the /test.txt fixture file
+        ;
+        $generator = $root->__invoke($request, $response);
+        $promise = \Amp\resolve($generator);
+        \Amp\wait($promise);
+
+        // Return so we can test cached responses in the next case
+        return $root;
+    }
+
+    /**
+     * @depends testBasicFileResponse
+     */
+    public function testCachedResponse($root) {
+        $request = $this->getMock('Aerys\Request');
+        $request->expects($this->once())
+            ->method("getUri")
+            ->will($this->returnValue("/test.txt"))
+        ;
+        $request->expects($this->any())
+            ->method("getMethod")
+            ->will($this->returnValue("GET"))
+        ;
+        $response = $this->getMock('Aerys\Response');
+        $response->expects($this->once())
+            ->method("end")
+            ->with("test") // <-- the contents of the /test.txt fixture file
+        ;
+        $root->__invoke($request, $response);
+    }
+
+    /**
+     * @depends testBasicFileResponse
+     */
+    public function testDebugModeIgnoresCacheIfCacheControlHeaderIndicatesToDoSo($root) {
+        $server = new class extends Server {
+            function __construct() {}
+            function attach(ServerObserver $obj){}
+            function detach(ServerObserver $obj){}
+            function getOption(string $option) { return true; }
+            function state(): int { return Server::STARTED; }
+        };
+        $root->update($server);
+
+        $request = $this->getMock('Aerys\Request');
+        $request->expects($this->once())
+            ->method("getUri")
+            ->will($this->returnValue("/test.txt"))
+        ;
+        $request->expects($this->once())
+            ->method("getHeaderArray")
+            ->will($this->returnValue(["no-cache"]))
+        ;
+        $request->expects($this->any())
+            ->method("getMethod")
+            ->will($this->returnValue("GET"))
+        ;
+        $response = $this->getMock('Aerys\Response');
+        $response->expects($this->once())
+            ->method("end")
+            ->with("test") // <-- the contents of the /test.txt fixture file
+        ;
+        $generator = $root->__invoke($request, $response);
+        $promise = \Amp\resolve($generator);
+        \Amp\wait($promise);
+
+        return $root;
+    }
+
+    /**
+     * @depends testDebugModeIgnoresCacheIfCacheControlHeaderIndicatesToDoSo
+     */
+    public function testDebugModeIgnoresCacheIfPragmaHeaderIndicatesToDoSo($root) {
+        $request = $this->getMock('Aerys\Request');
+        $request->expects($this->once())
+            ->method("getUri")
+            ->will($this->returnValue("/test.txt"))
+        ;
+        $request->expects($this->exactly(2))
+            ->method("getHeaderArray")
+            ->will($this->onConsecutiveCalls([], ["no-cache"]))
+        ;
+        $request->expects($this->any())
+            ->method("getMethod")
+            ->will($this->returnValue("GET"))
+        ;
+        $response = $this->getMock('Aerys\Response');
+        $response->expects($this->once())
+            ->method("end")
+            ->with("test") // <-- the contents of the /test.txt fixture file
+        ;
+        $generator = $root->__invoke($request, $response);
+        $promise = \Amp\resolve($generator);
+        \Amp\wait($promise);
+
+        return $root;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
