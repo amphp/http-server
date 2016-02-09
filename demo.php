edb78a11c4fd5b865608edd5e7ef731dbc846854
Bob Weinand <bobwei9@hotmail.com>
2016-02-09 15:44:48 +0100

    Prevent execution with PHP older than 7.0.0

--- demo.php
+++ demo.php
@@ -1,5 +1,9 @@
 <?php
 
+// <ignore>
+if (!class_exists('Aerys\Process')) { if (PHP_MAJOR_VERSION < 7) { echo "To run aerys, you need to run it with PHP 7.\n"; } echo "This file is not supposed to be invoked directly. To run it, use `php bin/aerys -c demo.php`.\n"; die(1); } eval(file_get_contents(__FILE__, null, null, __COMPILER_HALT_OFFSET__)); __halt_compiler();
+// </ignore>
+
 use Aerys\{ Host, Request, Response, Router, Websocket, function root, function router, function websocket };
 
 /* --- Global server options -------------------------------------------------------------------- */
