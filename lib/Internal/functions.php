03ced91fbc49b2cf2667d92345a082b34b3a9101
Max Meijer <meijermax99@gmail.com>
2017-10-01 11:01:14 +0200

    Throw descriptive error if no hosts are returned from the config file

--- lib/Internal/functions.php
+++ lib/Internal/functions.php
@@ -112,6 +112,12 @@ function bootServer(PsrLogger $logger, Console $console): \Generator {
         $hosts = [$hosts];
     }
 
+    if (empty($hosts)) {
+        throw new \Error(
+            "Config file at $configFile did not return any hosts"
+        );
+    }
+
     if (!defined("AERYS_OPTIONS")) {
         $options = [];
     } elseif (is_array(AERYS_OPTIONS)) {
