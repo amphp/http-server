5ad1015688131080762eb6cf8a6d450ff4d04313
Aaron Piotrowski <aaron@trowski.com>
2018-01-28 12:01:11 -0600

    Add Hello, world example

---
+++ examples/hello-world.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+use Aerys\CallableResponder;
+use Aerys\Request;
+use Aerys\Response;
+use Aerys\Server;
+
+// Run this script, then visit http://localhost:8080/?name=Your-name in your browser.
+
+Amp\Loop::run(function () {
+    $responder = new CallableResponder(function (Request $request) {
+        $name = $request->getUri()->getQueryParameter('name');
+        return new Response\TextResponse("Hello, " . ($name ?? "world") . "!");
+    });
+
+    $server = new Server($responder);
+    $server->expose("*", 8080);
+
+    // Stop the server when SIGINT is received (this is technically optional, but it is best to call Server::stop()).
+    Amp\Loop::onSignal(SIGINT, function (string $watcherId) use ($server) {
+        Amp\Loop::cancel($watcherId); // Cancel this watcher to automatically stop the loop.
+        $server->stop();
+    });
+
+    $server->start();
+});
