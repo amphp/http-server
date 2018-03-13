f3fdd6fe41865cce5c0f2cb9302d27fb6d0ccff4
Niklas Keller <me@kelunik.com>
2018-03-13 17:01:44 +0100

    Rename ./etc to ./resources

--- src/constants.php
+++ src/constants.php
@@ -4,10 +4,10 @@ namespace Amp\Http\Server;
 
 \define(
     __NAMESPACE__ . "\\DEFAULT_ERROR_HTML",
-    \file_get_contents(\dirname(__DIR__) . "/etc/error.html")
+    \file_get_contents(\dirname(__DIR__) . "/resources/error.html")
 );
 
 \define(
     __NAMESPACE__ . "\\INTERNAL_SERVER_ERROR_HTML",
-    \file_get_contents(\dirname(__DIR__) . "/etc/internal-server-error.html")
+    \file_get_contents(\dirname(__DIR__) . "/resources/internal-server-error.html")
 );
