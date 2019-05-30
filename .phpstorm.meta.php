3a195c9cf82bb237746c72e327f385becfedec84
asdfpowdfeuavxz <43964090+asdfpowdfeuavxz@users.noreply.github.com>
2019-05-30 16:04:34 +0200

    please use this
    i use this

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@
 namespace PHPSTORM_META {
     $STATIC_METHOD_TYPES = [
         \PHPUnit\Framework\TestCase::createMock("") => [
-            "" == "@",
+            "" => "@",
         ],
     ];
 }
