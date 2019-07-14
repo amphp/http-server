2058aaf1f919042e9bec662020ab770c96b2b877
Niklas Keller <me@kelunik.com>
2019-07-14 20:19:27 +0200

    Add TLS to hello-world example

--- examples/hello-world.php
+++ examples/hello-world.php
@@ -1,10 +1,9 @@
 #!/usr/bin/env php
 <?php
 
-require \dirname(__DIR__) . "/vendor/autoload.php";
+require dirname(__DIR__) . "/vendor/autoload.php";
 
 use Amp\ByteStream\ResourceOutputStream;
-use Amp\Http\Server\Request;
 use Amp\Http\Server\RequestHandler\CallableRequestHandler;
 use Amp\Http\Server\Response;
 use Amp\Http\Server\Server;
@@ -16,18 +15,22 @@ use Monolog\Logger;
 
 // Run this script, then visit http://localhost:1337/ in your browser.
 
-Amp\Loop::run(function () {
+Amp\Loop::run(static function () {
+    $cert = new Socket\Certificate(__DIR__ . '/../test/server.pem');
+
     $servers = [
         Socket\listen("0.0.0.0:1337"),
         Socket\listen("[::]:1337"),
+        Socket\listen("0.0.0.0:1338", null, (new Socket\ServerTlsContext)->withDefaultCertificate($cert)),
+        Socket\listen("[::]:1338", null, (new Socket\ServerTlsContext)->withDefaultCertificate($cert)),
     ];
 
-    $logHandler = new StreamHandler(new ResourceOutputStream(\STDOUT));
+    $logHandler = new StreamHandler(new ResourceOutputStream(STDOUT));
     $logHandler->setFormatter(new ConsoleFormatter);
     $logger = new Logger('server');
     $logger->pushHandler($logHandler);
 
-    $server = new Server($servers, new CallableRequestHandler(function (Request $request) {
+    $server = new Server($servers, new CallableRequestHandler(static function () {
         return new Response(Status::OK, [
             "content-type" => "text/plain; charset=utf-8"
         ], "Hello, World!");
@@ -36,7 +39,7 @@ Amp\Loop::run(function () {
     yield $server->start();
 
     // Stop the server when SIGINT is received (this is technically optional, but it is best to call Server::stop()).
-    Amp\Loop::onSignal(SIGINT, function (string $watcherId) use ($server) {
+    Amp\Loop::onSignal(SIGINT, static function (string $watcherId) use ($server) {
         Amp\Loop::cancel($watcherId);
         yield $server->stop();
     });
