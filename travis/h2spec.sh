76b7511936c4d2bc4fe4fdb141becfad1e6c6581
Aaron Piotrowski <aaron@trowski.com>
2019-07-22 13:52:43 -0500

    Move h2spec test to separate script

---
+++ travis/h2spec.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+travis_retry wget https://github.com/summerwind/h2spec/releases/latest/download/h2spec_linux_amd64.tar.gz
+tar -xzf h2spec_linux_amd64.tar.gz
+php ../test/test-server.php &
+SERVER_PID=$!
+./h2spec -p 1338 --tls --insecure --strict
+kill ${SERVER_PID}
