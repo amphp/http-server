89e26e2741e420994930555be17852e34da6beff
Aaron Piotrowski <aaron@trowski.com>
2019-07-23 10:04:43 -0500

    Fix test server script path

--- travis/h2spec.sh
+++ travis/h2spec.sh
@@ -2,7 +2,6 @@
 
 wget https://github.com/summerwind/h2spec/releases/latest/download/h2spec_linux_amd64.tar.gz
 tar -xzf h2spec_linux_amd64.tar.gz
-php ../test/test-server.php &
-SERVER_PID=$!
+php test/test-server.php &
+sleep 1
 ./h2spec -p 1338 --tls --insecure --strict
-kill ${SERVER_PID}
