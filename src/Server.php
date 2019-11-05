1892ae5226f92447a8d9984cef4ddd33b31b46b6
Aaron Piotrowski <aaron@trowski.com>
2019-11-05 09:16:13 -0600

    Define alias in autoloadable file

---
+++ src/Server.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Amp\Http\Server;
+
+// Alias for backward compatibility.
+\class_alias(HttpServer::class, Server::class);
--- src/functions.php
+++ src/functions.php
@@ -4,9 +4,6 @@ namespace Amp\Http\Server;
 
 use Amp\Http\Status;
 
-// Define class alias for backward compatibility.
-\class_alias(HttpServer::class, Server::class);
-
 function redirectTo(string $uri, int $statusCode = Status::FOUND): Response
 {
     return new Response($statusCode, ['location' => $uri]);
