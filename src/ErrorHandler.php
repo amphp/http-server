4ce7cc512af6630a073af5033fed8ac13caaf72e
Aaron Piotrowski <aaron@trowski.com>
2022-04-05 19:50:33 -0500

    Add missing explicit nullability

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -9,5 +9,5 @@ interface ErrorHandler
      * @param string|null $reason Reason message. Will use the status code's default reason if not provided.
      * @param Request|null $request Null if the error occurred before parsing the request completed.
      */
-    public function handleError(int $status, string $reason = null, Request $request = null): Response;
+    public function handleError(int $status, ?string $reason = null, ?Request $request = null): Response;
 }
