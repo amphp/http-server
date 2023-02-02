a2f94d055ba5907e4c60502b5380da075dbd9b92
Aaron Piotrowski <aaron@trowski.com>
2023-02-01 18:40:32 -0600

    Make error handler cache static

--- src/DefaultErrorHandler.php
+++ src/DefaultErrorHandler.php
@@ -5,39 +5,32 @@ namespace Amp\Http\Server;
 use Amp\Http\HttpStatus;
 
 /**
- * ErrorHandler instance used by default if none is given.
+ * Error handler that sends a simple HTML error page.
  */
 final class DefaultErrorHandler implements ErrorHandler
 {
-    /** @var string[] */
-    private array $cache = [];
+    private static ?string $errorHtml = null;
+
+    /** @var array<int, string> */
+    private static array $cache = [];
 
     public function handleError(int $status, ?string $reason = null, ?Request $request = null): Response
     {
-        static $errorHtml;
+        self::$errorHtml ??= \file_get_contents(\dirname(__DIR__) . "/resources/error.html");
 
-        if ($errorHtml === null) {
-            $errorHtml = \file_get_contents(\dirname(__DIR__) . "/resources/error.html");
-        }
+        $body = self::$cache[$status] ??= \str_replace(
+            ["{code}", "{reason}"],
+            // Using standard reason in HTML for caching purposes.
+            [$status, HttpStatus::getReason($status)],
+            self::$errorHtml,
+        );
 
-        if (!isset($this->cache[$status])) {
-            $this->cache[$status] = \str_replace(
-                ["{code}", "{reason}"],
-                // Using standard reason in HTML for caching purposes.
-                [$status, HttpStatus::getReason($status)],
-                $errorHtml
-            );
-        }
-
-        $response = new Response(
+        return new Response(
+            status: $status,
             headers: [
                 "content-type" => "text/html; charset=utf-8",
             ],
-            body: $this->cache[$status],
+            body: $body,
         );
-
-        $response->setStatus($status, $reason);
-
-        return $response;
     }
 }
