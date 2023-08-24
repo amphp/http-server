3c5e8fe900b6dd1e063b69f4090d07bc6f840ebf
Aaron Piotrowski <aaron@trowski.com>
2023-08-24 18:33:31 -0500

    Add previous to HttpErrorException

--- src/HttpErrorException.php
+++ src/HttpErrorException.php
@@ -4,9 +4,15 @@ namespace Amp\Http\Server;
 
 final class HttpErrorException extends \Exception
 {
-    public function __construct(private readonly int $status, private readonly ?string $reason = null)
-    {
-        parent::__construct('Error ' . $status . ($this->reason !== null && $this->reason !== '' ? ': ' . $reason : ''));
+    public function __construct(
+        private readonly int $status,
+        private readonly ?string $reason = null,
+        ?\Throwable $previous = null,
+    ) {
+        parent::__construct(
+            message: 'Error ' . $status . ($this->reason !== null && $this->reason !== '' ? ': ' . $reason : ''),
+            previous: $previous,
+        );
     }
 
     public function getReason(): ?string
