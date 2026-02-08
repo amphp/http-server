5254e24004ac623620c4759c16cf10510e73cd61
Aaron Piotrowski <aaron@trowski.com>
2026-02-08 13:03:50 -0600

    Refactor stream reset protection to check reset counts over a time interval

--- src/Driver/Http2Driver.php
+++ src/Driver/Http2Driver.php
@@ -33,6 +33,14 @@ use function Amp\Http\formatDateHeader;
 
 final class Http2Driver implements HttpDriver, Http2Processor
 {
+    /** Stream behavior window interval in seconds. */
+    private const STREAM_BEHAVIOR_WINDOW = 10;
+    /** Ratio of normally released streams to exceptionally released which must be exceeded to automatically
+     * disconnect the client.*/
+    private const RESET_STREAM_RATIO = 0.25;
+    /** Number of streams released within the behavior window for the reset ratio to be applicable. */
+    private const STREAM_BEHAVIOR_THRESHOLD = 10;
+
     public const DEFAULT_MAX_FRAME_SIZE = 1 << 14;
     public const DEFAULT_WINDOW_SIZE = (1 << 16) - 1;
 
@@ -92,6 +100,14 @@ final class Http2Driver implements HttpDriver, Http2Processor
 
     /** @var int[] Map of request hashes to stream IDs. */
     private $streamIdMap = [];
+
+    /** @var array<int, int> Release timestamps of the last {@see STREAM_BEHAVIOR_WINDOW} released streams. */
+    private $releasedStreams = [];
+
+    /** @var array<int, int> Release timestamps of the last {@see STREAM_BEHAVIOR_WINDOW} streams released due to
+     *      an exception (such as the client resetting the stream or sending an invalid frame).
+     */
+    private $exceptionalStreams = [];
 
     /** @var int[] Map of URLs pushed on this connection. */
     private $pushCache = [];
@@ -569,6 +585,9 @@ final class Http2Driver implements HttpDriver, Http2Processor
     {
         \assert(isset($this->streams[$id]), "Tried to release a non-existent stream");
 
+        $exceptional = $exception !== null;
+        $clientInitiated = $id & 1;
+
         if (isset($this->bodyEmitters[$id])) {
             $emitter = $this->bodyEmitters[$id];
             unset($this->bodyEmitters[$id]);
@@ -583,8 +602,34 @@ final class Http2Driver implements HttpDriver, Http2Processor
 
         unset($this->streams[$id]);
 
-        if ($id & 1) { // Client-initiated stream.
-            $this->remainingStreams++;
+        if (!$clientInitiated) {
+            return; // Additional checks unnecessary for server-initiated streams.
+        }
+
+        $this->remainingStreams++;
+
+        $now = \time();
+        $filterCallback = function (int $releasedAt) use ($now): bool {
+            return $releasedAt > $now - self::STREAM_BEHAVIOR_WINDOW;
+        };
+        $this->releasedStreams = \array_filter($this->releasedStreams, $filterCallback);
+        $this->releasedStreams[$id] = $now;
+
+        if ($exceptional) {
+            $this->exceptionalStreams = \array_filter($this->exceptionalStreams, $filterCallback);
+            $this->exceptionalStreams[$id] = $now;
+
+            $releasedStreamCount = \count($this->releasedStreams);
+            $exceptionalStreamCount = \count($this->exceptionalStreams);
+
+            if ($releasedStreamCount >= self::STREAM_BEHAVIOR_THRESHOLD
+                && $exceptionalStreamCount / $releasedStreamCount > self::RESET_STREAM_RATIO
+            ) {
+                $this->handleConnectionException(new Http2ConnectionException(
+                    'Too many reset streams',
+                    Http2Parser::ENHANCE_YOUR_CALM
+                ));
+            }
         }
     }
 
@@ -805,9 +850,16 @@ final class Http2Driver implements HttpDriver, Http2Processor
                 );
             }
         } else {
-            if (!($streamId & 1) || $this->remainingStreams-- <= 0 || $streamId <= $this->remoteStreamId) {
+            if (!($streamId & 1) || $this->remainingStreams-- <= 0) {
                 throw new Http2ConnectionException(
                     "Invalid stream ID",
+                    Http2Parser::PROTOCOL_ERROR
+                );
+            }
+
+            if ($this->remainingStreams-- <= 0) {
+                throw new Http2ConnectionException(
+                    "Concurrent stream limit exceeded",
                     Http2Parser::PROTOCOL_ERROR
                 );
             }
